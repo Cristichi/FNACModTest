9bde440d754a49e2982475d3ca5514ec4589a2cf
Cristichi <cristichi@hotmail.es>
2025-01-01 17:46:19 +0100

    Initial commit.

---
+++ src/main/java/es/cristichi/fnacmodtest/Main.java
@@ -0,0 +1,42 @@
+package es.cristichi.fnacmodtest;
+
+import es.cristichi.fnacmodtest.anims.Spyro;
+import es.cristichi.fnac.exception.NightException;
+import es.cristichi.fnac.gui.MenuJC;
+import es.cristichi.fnac.gui.NightJC;
+import es.cristichi.fnac.io.Settings;
+import es.cristichi.fnac.obj.Jumpscare;
+import es.cristichi.fnac.obj.cams.CrisRestaurantMap;
+import es.cristichi.fnac.obj.nights.NightFactory;
+import es.cristichi.fnac.obj.nights.NightRegistry;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        NightRegistry.registerNight(0, new NightFactory() {
+            @Override
+            public MenuJC.Item getItem() {
+                return new MenuJC.Item("night1", "Let's play with Spyro", "Night 1", null);
+            }
+            
+            @Override
+            public NightJC createNight(Settings settings, Jumpscare powerOutage,
+                                       Random rng) throws IOException, NightException {
+                CrisRestaurantMap nightMap = new CrisRestaurantMap();
+                nightMap.addCamAnimatronics("kitchen", new Spyro("spyro", Map.of(0,20), List.of(
+                        List.of("kitchen", "dining area", "corridor 2", "bathrooms", "rightDoor"),
+                        List.of("storage", "dining area", "main stage", "corridor 3", "leftDoor")
+                ), Color.MAGENTA.darker(), rng));
+                return new NightJC("Spyro's Night", settings.getFps(), nightMap, "night/n1/paper.png", powerOutage, rng, 20,
+                        .45f, "night/general/completed.wav");
+            }
+        });
+        
+        new es.cristichi.fnac.Main().run();
+    }
+}
---
+++ src/main/java/es/cristichi/fnacmodtest/anims/Spyro.java
@@ -0,0 +1,27 @@
+package es.cristichi.fnacmodtest.anims;
+
+import es.cristichi.fnac.exception.ResourceException;
+import es.cristichi.fnac.io.Resources;
+import es.cristichi.fnac.obj.Jumpscare;
+import es.cristichi.fnac.obj.JumpscareVisualSetting;
+import es.cristichi.fnac.obj.anim.PathedMoveAnimatronicDrawing;
+import es.cristichi.fnac.obj.cnight.CustomNightAnimatronic;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+@CustomNightAnimatronic(name = "Spyro", portraitPath = "anim/spyro/portrait.png",
+                        tutDesc = "Nope",
+                        restDesc = "Spyro goes to right door from bathrooms.")
+public class Spyro extends PathedMoveAnimatronicDrawing {
+    public Spyro(String nameId, Map<Integer, Integer> aiDuringNight, List<List<String>> camPaths, Color debugColor,
+                 Random rng) throws ResourceException {
+        super(nameId, 3, 5, aiDuringNight, GENERIC_MAX_AI, false, false,
+                "anim/spyro/camImg.png", new Jumpscare("anim/spyro/jump.gif", 0,
+                        Resources.loadSound("anim/spyro/jump.wav", "spyroJump.wav"), 0,
+                        JumpscareVisualSetting.STRETCHED), camPaths, debugColor, rng);
+        sounds.put("move", Resources.loadSound("anim/spyro/move.wav", "spyroMove.wav"));
+    }
+}
