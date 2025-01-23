054cb7d07f1c1ccb23b91a4f029fe27ff5beee76
Cristichi <cristichi@hotmail.es>
2025-01-23 13:39:20 +0100

    Updated FNAC to v0.8.13. v1.2-SNAPSHOT

--- src/main/java/es/cristichi/fnacmodtest/Main.java
+++ src/main/java/es/cristichi/fnacmodtest/Main.java
@@ -13,6 +13,7 @@ import es.cristichi.fnac.gui.MenuJC;
 import es.cristichi.fnac.gui.NightJC;
 import es.cristichi.fnac.io.Resources;
 import es.cristichi.fnac.io.Settings;
+import es.cristichi.fnac.loading.LoadRunnable;
 import es.cristichi.fnac.nights.NightFactory;
 import es.cristichi.fnac.nights.NightRegistry;
 import es.cristichi.fnacmodtest.anims.Spyro;
@@ -24,102 +25,120 @@ import java.util.Map;
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) throws ResourceException {
+    public static void main(String[] args) {
+        // We should not change these values during loading, we do it right away.
         FnacMain.GAME_TITLE = "FNAC: Official modded example (by Cristichi)";
         FnacMain.DEBUG_ALLNIGHTS = true;
         
-        CustomNightAnimRegistry.registerAnimatronic(new CustomNightAnimFactory<Spyro>("Spyro", """
-                Spyro goes to the right door. If he is at "bathrooms" or "cam3", he teleports \
-                directly to the right door to scare you.""",
-                20, Resources.loadImage("anim/spyro/portrait.png"), new String[]{"storage", "cam1"}) {
-            @Override
-            public Spyro generate(CustomNightAnimData data, Random rng) throws ResourceException {
-                try {
-                    return new Spyro("Spyro", Map.of(0, data.ai()),
-                            List.of(
-                                    List.of("cam1", "cam3", "rightDoor"),
-                                    List.of("cam2", "cam4", "leftDoor"),
-                                    List.of("kitchen", "dining area", "corridor 2", "bathrooms", "rightDoor"),
-                                    List.of("storage", "dining area", "main stage", "corridor 3", "leftDoor")
-                            ), rng);
-                } catch (MalformedURLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
+        final RestaurantCamMapFactory restaurantCamMapFactory = new RestaurantCamMapFactory();
         
-        // Nights
-        RestaurantCamMapFactory restaurantCamMapFactory = new RestaurantCamMapFactory();
-        /* This makes custom Night available instead of the Tutorial. Since the Custom Night also saves itself
-         * as a completed Night, this still allows the rest of the Nights to occur.
-         */
-        //NightRegistry.registerNight(0, null);
-        // Night 1.
-        NightRegistry.registerNight(0, new NightFactory() {
-            @Override
-            public MenuJC.Item getItem() {
-                return new MenuJC.Item("night1", "Let's play with Spyro", "Night 1", null);
-            }
-            
-            @Override
-            public NightJC createNight(Settings settings, Jumpscare powerOutage,
-                                       Random rng) throws IOException, NightException {
-                CameraMap nightMap = restaurantCamMapFactory.generate();
-                nightMap.addCamAnimatronics("kitchen", new Spyro("spyro", Map.of(0,10), List.of(
-                        List.of("kitchen", "dining area", "corridor 2", "bathrooms", "rightDoor"),
-                        List.of("storage", "dining area", "main stage", "corridor 3", "leftDoor")
-                ), rng));
-                return new NightJC("Spyro's Night", settings.getFps(), nightMap,
-                        Resources.loadImage("night/n1/paper.png"), powerOutage, rng, 20,
-                        .45f, Resources.loadSound("night/general/completed.wav"), null, null);
-            }
-        });
-        // I copied Night 2 of this version of FNAC from its source files and added Spyro to the kitchen.
-        NightRegistry.registerNight(2, new NightFactory() {
-            
-            @Override
-            public MenuJC.Item getItem() throws ResourceException {
-                return new MenuJC.Item("n2", "Continue", "Night 2",
-                        Resources.loadImage("night/n2/loading.jpg"));
-            }
-            
-            @Override
-            public NightJC createNight(Settings settings, Jumpscare powerOutage,
-                                       Random rng) throws IOException, NightException {
-                AnimatronicDrawing bob = new RoamingBob("Bob", Map.of(0,4), false, false,
-                        List.of("corridor 2", "corridor 4", "bathrooms", "offices"), rng);
-                
-                AnimatronicDrawing maria = new RoamingMaria("Maria", Map.of(1,1, 4,2), false, false,
-                        List.of("corridor 1", "corridor 3", "staff lounge"), rng);
-                
-                AnimatronicDrawing paco = new Paco("Paco", Map.of(0,4, 4,5), false, true,
-                        List.of(
-                                List.of("kitchen", "dining area", "corridor 1", "corridor 3", "leftDoor"),
-                                List.of("kitchen", "dining area", "corridor 2", "corridor 4", "rightDoor")
-                        ), rng);
-                
-                AnimatronicDrawing crisIsClose = new RoamingCris("Cris", Map.of(0,1, 4,2, 5,3), true, false,
-                        List.of("kitchen", "storage", "main stage", "staff lounge", "bathrooms"), rng);
-                
-                Spyro spyro = new Spyro("spyro", Map.of(0,2, 3,5),
-                        List.of(
-                            List.of("kitchen", "dining area", "corridor 2", "bathrooms", "rightDoor"),
-                            List.of("storage", "dining area", "main stage", "corridor 3", "leftDoor")
-                        ), rng);
-                
-                CameraMap nightMap = restaurantCamMapFactory.generate();
-                nightMap.addCamAnimatronics("kitchen", paco, spyro);
-                nightMap.addCamAnimatronics("storage", bob);
-                nightMap.addCamAnimatronics("offices", maria);
-                nightMap.addCamAnimatronics("staff lounge", crisIsClose);
-                
-                return new NightJC("Night 2", settings.getFps(), nightMap,
-                        Resources.loadImage("night/n2/paper.png"), powerOutage, rng, 90, 0.45f,
-                        Resources.loadSound("night/general/completed.wav"), null, null);
-            }
-        });
-        // All other Nights would be the same.
+        /* We place each thing we need to load in an individual LoadRunnable instance, so that each of them
+        * load while the player is seeing the loading screen, and also so that they load in individual Threads.*/
+        LoadRunnable[] loadingRunnables = new LoadRunnable[]{
+                () -> CustomNightAnimRegistry.registerAnimatronic(new CustomNightAnimFactory<Spyro>("Spyro", """
+                        Spyro goes to the right door. If he is at "bathrooms" or "cam3", he teleports \
+                        directly to the right door to scare you.""", 20, Resources.loadImage("anim/spyro/portrait.png"),
+                        new String[]{"storage", "cam1"}) {
+                    @Override
+                    public Spyro generate(CustomNightAnimData data, Random rng) throws ResourceException {
+                        try {
+                            return new Spyro("Spyro", Map.of(0, data.ai()),
+                                    List.of(List.of("cam1", "cam3", "rightDoor"), List.of("cam2", "cam4", "leftDoor"),
+                                            List.of("kitchen", "dining area", "corridor 2", "bathrooms", "rightDoor"),
+                                            List.of("storage", "dining area", "main stage", "corridor 3", "leftDoor")),
+                                    rng);
+                        } catch (MalformedURLException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }),
+                /* This replaces the Tutorial, because the Tutorial is usually loaded on 0 comlpeted Nights. */
+                () -> NightRegistry.registerNight(0, new NightFactory() {
+                    @Override
+                    public MenuJC.Item getItem() {
+                        return new MenuJC.Item("night0", "Let's play with Spyro", "Night 1", null);
+                    }
+                    
+                    @Override
+                    public NightJC createNight(Settings settings, Jumpscare powerOutage,
+                                               Random rng) throws IOException, NightException {
+                        CameraMap nightMap = restaurantCamMapFactory.generate();
+                        nightMap.addCamAnimatronics("kitchen", new Spyro("spyro", Map.of(0,10), List.of(
+                                List.of("kitchen", "dining area", "corridor 2", "bathrooms", "rightDoor"),
+                                List.of("storage", "dining area", "main stage", "corridor 3", "leftDoor")
+                        ), rng));
+                        return new NightJC("Spyro's Night", settings.getFps(), nightMap,
+                                Resources.loadImage("night/n1/paper.png"), powerOutage, rng, 20,
+                                .45f, Resources.loadSound("night/general/completed.wav"), null, null);
+                    }
+                }),
+                // Night 1.
+                () -> NightRegistry.registerNight(1, new NightFactory() {
+                    @Override
+                    public MenuJC.Item getItem() {
+                        return new MenuJC.Item("night1", "Let's play with Spyro", "Night 1", null);
+                    }
+                    
+                    @Override
+                    public NightJC createNight(Settings settings, Jumpscare powerOutage,
+                                               Random rng) throws IOException, NightException {
+                        CameraMap nightMap = restaurantCamMapFactory.generate();
+                        nightMap.addCamAnimatronics("kitchen", new Spyro("spyro", Map.of(0,10), List.of(
+                                List.of("kitchen", "dining area", "corridor 2", "bathrooms", "rightDoor"),
+                                List.of("storage", "dining area", "main stage", "corridor 3", "leftDoor")
+                        ), rng));
+                        return new NightJC("Spyro's Night", settings.getFps(), nightMap,
+                                Resources.loadImage("night/n1/paper.png"), powerOutage, rng, 20,
+                                .45f, Resources.loadSound("night/general/completed.wav"), null, null);
+                    }
+                }),
+                // Night 2.
+                () -> NightRegistry.registerNight(2, new NightFactory() {
+                    
+                    @Override
+                    public MenuJC.Item getItem() throws ResourceException {
+                        return new MenuJC.Item("n2", "Continue", "Night 2",
+                                Resources.loadImage("night/n2/loading.jpg"));
+                    }
+                    
+                    @Override
+                    public NightJC createNight(Settings settings, Jumpscare powerOutage,
+                                               Random rng) throws IOException, NightException {
+                        AnimatronicDrawing bob = new RoamingBob("Bob", Map.of(0,4), false, false,
+                                List.of("corridor 2", "corridor 4", "bathrooms", "offices"), rng);
+                        
+                        AnimatronicDrawing maria = new RoamingMaria("Maria", Map.of(1,1, 4,2), false, false,
+                                List.of("corridor 1", "corridor 3", "staff lounge"), rng);
+                        
+                        AnimatronicDrawing paco = new Paco("Paco", Map.of(0,4, 4,5), false, true,
+                                List.of(
+                                        List.of("kitchen", "dining area", "corridor 1", "corridor 3", "leftDoor"),
+                                        List.of("kitchen", "dining area", "corridor 2", "corridor 4", "rightDoor")
+                                ), rng);
+                        
+                        AnimatronicDrawing crisIsClose = new RoamingCris("Cris", Map.of(0,1, 4,2, 5,3), true, false,
+                                List.of("kitchen", "storage", "main stage", "staff lounge", "bathrooms"), rng);
+                        
+                        Spyro spyro = new Spyro("spyro", Map.of(0,2, 3,5),
+                                List.of(
+                                        List.of("kitchen", "dining area", "corridor 2", "bathrooms", "rightDoor"),
+                                        List.of("storage", "dining area", "main stage", "corridor 3", "leftDoor")
+                                ), rng);
+                        
+                        CameraMap nightMap = restaurantCamMapFactory.generate();
+                        nightMap.addCamAnimatronics("kitchen", paco, spyro);
+                        nightMap.addCamAnimatronics("storage", bob);
+                        nightMap.addCamAnimatronics("offices", maria);
+                        nightMap.addCamAnimatronics("staff lounge", crisIsClose);
+                        
+                        return new NightJC("Night 2", settings.getFps(), nightMap,
+                                Resources.loadImage("night/n2/paper.png"), powerOutage, rng, 90, 0.45f,
+                                Resources.loadSound("night/general/completed.wav"), null, null);
+                    }
+                })
+        };
         
-        new es.cristichi.fnac.FnacMain().run();
+        // We start the game, specifying the methods that must be run during loading.
+        new es.cristichi.fnac.FnacMain().run(loadingRunnables, null, null);
     }
 }
