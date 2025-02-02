3682fb0f60e51ba67a6d42ed0cd63f6f74118e13
Cristichi <cristichi@hotmail.es>
2025-02-02 15:21:11 +0100

    v1.5. Updated FNAC to v0.9.0.

--- src/main/java/es/cristichi/fnacmodtest/anims/Spyro.java
+++ src/main/java/es/cristichi/fnacmodtest/anims/Spyro.java
@@ -4,7 +4,9 @@ import es.cristichi.fnac.anim.Jumpscare;
 import es.cristichi.fnac.anim.JumpscareVisualSetting;
 import es.cristichi.fnac.anim.PathedMoveAnimatronicDrawing;
 import es.cristichi.fnac.exception.ResourceException;
+import es.cristichi.fnac.io.NightDrawableImage;
 import es.cristichi.fnac.io.Resources;
+import es.cristichi.fnac.io.StaticNightDrawableImage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,18 +24,17 @@ public class Spyro extends PathedMoveAnimatronicDrawing {
     
     public Spyro(String nameId, Map<Integer, Integer> aiDuringNight, List<List<String>> camPaths,
                  Random rng) throws ResourceException, MalformedURLException {
-        super(nameId, 3, 5, aiDuringNight, false, false,
-                getFromURLorResources(),
-                new Jumpscare(Resources.loadGif("anim/spyro/jump.gif"), 0,Resources.loadSound("anim/spyro/jump.wav"),
-                        0, JumpscareVisualSetting.CENTERED),
-                camPaths, Color.MAGENTA.darker(), rng);
+        super(nameId, 3, 5, aiDuringNight, false, false, getFromUrlOrResources(),
+                new Jumpscare(Resources.loadGif("anim/spyro/jump.gif", false), 0,
+                        Resources.loadSound("anim/spyro/jump.wav"), 0, JumpscareVisualSetting.CENTERED), camPaths,
+                Color.MAGENTA.darker(), rng);
         sounds.put("move", Resources.loadSound("anim/spyro/move.wav"));
     }
     
-    private static BufferedImage getFromURLorResources() throws ResourceException {
+    private static NightDrawableImage getFromUrlOrResources() throws ResourceException {
         URL url = null;
         try {
-            url = new URL("https://upload.wikimedia.org/wikipedia/en/5/53/Spyro.png");
+            url = new URL("https://upload.wikimedia.org/wikipedia/en/5/53/Spyro.pngs");
             Image img = ImageIO.read(url);
             
             BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null),
@@ -43,10 +44,10 @@ public class Spyro extends PathedMoveAnimatronicDrawing {
             bGr.drawImage(img, 0, 0, null);
             bGr.dispose();
             
-            return bimage;
-        } catch (Exception e){
-            LOGGER.warn("Could not load URL {}. Loading from resource instead.", url==null?"null":url, e);
-            return Resources.loadImage("anim/spyro/camImg.png");
+            return new StaticNightDrawableImage(bimage);
+        } catch (Exception e) {
+            LOGGER.warn("Could not load URL {}. Loading from resource instead.", url == null ? "null" : url, e);
+            return Resources.loadGif("anim/spyro/cam.gif", true);
         }
     }
 }
